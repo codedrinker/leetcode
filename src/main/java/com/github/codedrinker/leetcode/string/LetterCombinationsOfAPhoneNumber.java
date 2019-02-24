55ea546c5eb67690efe073edcb25e7cd86705b03
codedrinker <gwangchunlei@gmail.com>
2019-02-24 18:39:20 +0800

    思路阻塞 完成一半

---
+++ src/main/java/com/github/codedrinker/leetcode/string/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,33 @@
+package com.github.codedrinker.leetcode.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by codedrinker on 2019/2/22.
+ * 未完成，思路阻塞，回来再看
+ */
+public class LetterCombinationsOfAPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        char[] chars = digits.toCharArray();
+        char[][] result = new char[(int) Math.pow(3, chars.length)][chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            int j = (int) Math.pow(3, chars.length) / 3;
+            for (int m = 0; m < j; m++) {
+                result[m][i] = (char) ((Character.digit(chars[i], 10) - 2) * 3 + 97);
+                result[m + j][i] = (char) ((Character.digit(chars[i], 10) - 2) * 3 + 97 + 1);
+                result[m + 2 * j][i] = (char) ((Character.digit(chars[i], 10) - 2) * 3 + 97 + 2);
+            }
+        }
+        List<String> letterCombinations = new ArrayList<>();
+        for (int i = 0; i < result.length; i++) {
+            letterCombinations.add(new String(result[i]));
+        }
+        return letterCombinations;
+    }
+
+    public static void main(String[] args) {
+        List<String> letterCombinations = new LetterCombinationsOfAPhoneNumber().letterCombinations("23");
+        System.out.println(letterCombinations);
+    }
+}
