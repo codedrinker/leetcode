2e9213fd546b8dcdc3946d5860cdd20f5e8373d7
codedrinker <gwangchunlei@gmail.com>
2019-05-21 22:55:09 +0800

    maxmum depth of binary tree

---
+++ src/main/java/com/github/codedrinker/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,23 @@
+package com.github.codedrinker.leetcode.tree;
+
+/**
+ * Created by codedrinker on 2019/5/21.
+ */
+public class MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
