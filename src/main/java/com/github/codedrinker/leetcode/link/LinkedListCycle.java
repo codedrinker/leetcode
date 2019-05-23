f23322fff0c003a832bc7d948afde72b46c2e739
codedrinker <gwangchunlei@gmail.com>
2019-05-23 23:39:17 +0800

    linked list cycle

---
+++ src/main/java/com/github/codedrinker/leetcode/link/LinkedListCycle.java
@@ -0,0 +1,33 @@
+package com.github.codedrinker.leetcode.link;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by codedrinker on 2019/5/23.
+ */
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> contains = new HashSet<>();
+        while (head != null) {
+            if (contains.contains(head)) {
+                return true;
+            } else {
+                contains.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
