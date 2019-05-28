1dc0f35dac53e9071e289dcd08a1fd75e6ec3896
codedrinker <gwangchunlei@gmail.com>
2019-05-28 22:04:20 +0800

    reverse linked list

---
+++ src/main/java/com/github/codedrinker/leetcode/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package com.github.codedrinker.leetcode.linkedlist;
+
+/**
+ * Created by codedrinker on 2019/5/27.
+ */
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
