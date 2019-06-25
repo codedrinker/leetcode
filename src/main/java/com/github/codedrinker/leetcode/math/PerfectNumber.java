db8ead0e36fa43291a0a45bb80076222f2e2bc41
codedrinker <gwangchunlei@gmail.com>
2019-06-25 23:04:01 +0800

    perfect number

---
+++ src/main/java/com/github/codedrinker/leetcode/math/PerfectNumber.java
@@ -0,0 +1,24 @@
+package com.github.codedrinker.leetcode.math;
+
+/**
+ * Created by codedrinker on 2019/6/25.
+ */
+//TODO
+public class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum - num == num;
+    }
+}
