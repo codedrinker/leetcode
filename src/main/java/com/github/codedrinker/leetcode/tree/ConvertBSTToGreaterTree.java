ad1d6ba5e7867eb991c9fce5ccefc57fc439f452
codedrinker <gwangchunlei@gmail.com>
2019-06-25 23:28:54 +0800

    convert bst to greater tree

---
+++ src/main/java/com/github/codedrinker/leetcode/tree/ConvertBSTToGreaterTree.java
@@ -0,0 +1,29 @@
+package com.github.codedrinker.leetcode.tree;
+
+/**
+ * Created by codedrinker on 2019/6/25.
+ */
+public class ConvertBSTToGreaterTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
