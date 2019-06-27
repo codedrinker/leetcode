8e17b48eea755276b0af108ce3639d43d58a569a
codedrinker <gwangchunlei@gmail.com>
2019-06-27 22:33:52 +0800

    diameter of binary tree

---
+++ src/main/java/com/github/codedrinker/leetcode/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,37 @@
+package com.github.codedrinker.leetcode.tree;
+
+/**
+ * Created by codedrinker on 2019/6/27.
+ */
+public class DiameterOfBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        depth(root);
+        return ans;
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
